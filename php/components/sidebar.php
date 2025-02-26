f2ce27a1ef2c73cf6c15a21fe38e738a68c67dc9
marumaru-3 <yukinaka3009@gmail.com>
2025-02-27 08:36:44 +0900

    0227-01

--- php/components/sidebar.php
+++ php/components/sidebar.php
@@ -11,14 +11,14 @@
     <ul class="sidebar__list">
       <li class="sidebar__item">
         <a href="/weight-management/home"
-          class="sidebar__link btn btn--nav sidebar__link--active">
+          class="sidebar__link btn btn--nav <?php echo $page === 'home' ? 'sidebar__link--active' : ''; ?>">
           <span class="material-symbols-outlined"> home </span>
           <span class="btn__text">ホーム</span>
         </a>
       </li>
       <li class="sidebar__item">
         <a href="/weight-management/log"
-          class="sidebar__link btn btn--nav">
+          class="sidebar__link btn btn--nav <?php echo $page === 'log' ? 'sidebar__link--active' : ''; ?>">
           <span class="material-symbols-outlined">
             accessibility_new
           </span>
@@ -27,7 +27,7 @@
       </li>
       <li class="sidebar__item">
         <a href="/weight-management/user"
-          class="sidebar__link btn btn--nav">
+          class="sidebar__link btn btn--nav <?php echo $page === 'user' ? 'sidebar__link--active' : ''; ?>">
           <span class="material-symbols-outlined"> account_circle </span>
           <span class="btn__text sp-none">ユーザー情報</span>
           <span class="btn__text sp-only">ユーザー</span>
@@ -35,14 +35,14 @@
       </li>
       <li class="sidebar__item">
         <a href="/weight-management/help"
-          class="sidebar__link btn btn--nav">
+          class="sidebar__link btn btn--nav <?php echo $page === 'help' ? 'sidebar__link--active' : ''; ?>">
           <span class="material-symbols-outlined"> help </span>
           <span class="btn__text">ヘルプ</span>
         </a>
       </li>
       <li class="sidebar__item">
         <a href="/weight-management/settings"
-          class="sidebar__link btn btn--nav">
+          class="sidebar__link btn btn--nav <?php echo $page === 'settings' ? 'sidebar__link--active' : ''; ?>">
           <span class="material-symbols-outlined"> settings </span>
           <span class="btn__text">設定</span>
         </a>
