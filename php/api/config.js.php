4bc81805570ec9d91520dbbb732ac62346d8a0a1
marumaru-3 <yukinaka3009@gmail.com>
2025-04-06 01:34:12 +0900

    fix: config.js.php の修正

--- php/api/config.js.php
+++ php/api/config.js.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once __DIR__ . "/bootstrap.php";
+
+$basePath = str_replace('/php/api/config.js.php', '', $scriptName);
+
 header("Content-Type: application/javascript");
 
-echo "const BASE_PATH = '" . BASE_CONTEXT_PATH . "';";
+echo "const BASE_PATH = '" . $basePath . "/';";
 echo "let showIdModal = " .
     ($_SESSION["show_id_modal"] ? "true" : "false") .
     ";";
