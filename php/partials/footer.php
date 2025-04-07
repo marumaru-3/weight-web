f7a0e55e2e67ac5d91c7d0a8012093ed3d165f92
marumaru-3 <yukinaka3009@gmail.com>
2025-04-07 19:51:49 +0900

    fix: google icon ロード時表示修正4

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -5,7 +5,7 @@ namespace partials;
 function footer()
 {
 ?>
-  <span class="material-symbols-outlined d-none" data-icon="content_copy"></span>
+  <span class="material-symbols-outlined must-hidden" data-icon="content_copy"></span>
   <script src="<?php echo BASE_CONTEXT_PATH; ?>php/api/config.js.php"></script>
   <script src="<?php echo BASE_JS_PATH; ?>helper.js"></script>
   <script type="module"
