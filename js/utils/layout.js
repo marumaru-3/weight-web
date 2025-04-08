2ba99bfd1d6977d0b211887f79818736e7508670
marumaru <yukinaka3009@gmail.com>
2025-04-08 10:11:59 +0900

    revert: 4コミット前の状態を反映

--- js/utils/layout.js
+++ js/utils/layout.js
@@ -61,12 +61,4 @@ export const initLayout = () => {
       }
     }
   });
-
-  // SPのときフッターの位置制御
-  const setVhUnit = () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--sidebar-sp_vh", `${vh}px`);
-  };
-  setVhUnit();
-  window.addEventListener("resize", setVhUnit);
 };
