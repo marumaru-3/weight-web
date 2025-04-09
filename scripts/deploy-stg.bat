69a4160c1e7752d6989f66d770c8eb7ffe901c90
marumaru-3 <yukinaka3009@gmail.com>
2025-04-10 08:37:35 +0900

    chore: ステージング用バッチを日本語対応に変更

--- scripts/deploy-stg.bat
+++ scripts/deploy-stg.bat
@@ -1,14 +1,16 @@
 ﻿@echo off
+chcp 65001
+
 echo === ステージング用に差し替えます ===
 
-copy /Y "..\htaccess.heroku-stg" "..\public\.htaccess"
+copy /Y ".htaccess.heroku-stg" "public\.htaccess"
 
 git add public/.htaccess
 git commit -m "chore: ステージング用の .htaccess に差し替え"
 git push heroku-stg main
 
 echo === ローカル用に戻します ===
-copy /Y "..\htaccess.local" "..\public\.htaccess"
+copy /Y ".htaccess.local" "public\.htaccess"
 
 echo === 作業完了！お疲れさまでした ===
 pause
