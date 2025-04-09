64e8bd5781571079e079df229634e4b6e4426d43
marumaru <yukinaka3009@gmail.com>
2025-04-09 10:14:16 +0900

    chore: .envのmodeによってViteのbaseパスを切り替える処理を追加

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-export default {
-  root: ".",
-  base: "/weight-web/public/assets/",
-  build: {
-    outDir: "public/assets",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: "src/main.js",
-      output: {
-        entryFileNames: "main.js",
-        chunkFileNames: "[name]-[hash].js",
-        assetFileNames: "style.css",
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    root: ".",
+    base: env.VITE_BASE_PATH || "/public/assets/",
+    build: {
+      outDir: "public/assets",
+      emptyOutDir: true,
+      rollupOptions: {
+        input: "src/main.js",
+        output: {
+          entryFileNames: "main.js",
+          chunkFileNames: "[name]-[hash].js",
+          assetFileNames: "style.css",
+        },
       },
     },
-  },
-};
+  };
+});
