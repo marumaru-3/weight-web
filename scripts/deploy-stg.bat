4c0569e15c75acc1f6c1144e4e7318cd403cb00a
marumaru <yukinaka3009@gmail.com>
2025-04-10 18:02:03 +0900

    chore: .htaccess構成を認証あり/なしで整理

--- scripts/deploy-stg.bat
+++ scripts/deploy-stg.bat
@@ -3,14 +3,14 @@ chcp 65001
 
 echo === ステージング用に差し替えます ===
 
-copy /Y ".htaccess.heroku-stg" "public\.htaccess"
+copy /Y ".htaccess.staging" "public\.htaccess"
 
 git add public/.htaccess
 git commit -m "chore: ステージング用の .htaccess に差し替え"
 git push heroku-stg main
 
 echo === ローカル用に戻します ===
-copy /Y ".htaccess.local" "public\.htaccess"
+copy /Y ".htaccess.public" "public\.htaccess"
 
 echo === 作業完了！お疲れさまでした ===
 pause
