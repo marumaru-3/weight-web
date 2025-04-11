66003de23f8a791cbf01c3b3ebcb12eb1128b5e2
marumaru <yukinaka3009@gmail.com>
2025-04-11 16:23:31 +0900

    fix: cookie削除後のエラーを解決

--- public/api/config.js.php
+++ public/api/config.js.php
@@ -7,7 +7,6 @@ $basePath = str_replace("/api/config.js.php", "", $scriptName);
 header("Content-Type: application/javascript");
 
 echo "const BASE_PATH = '" . $basePath . "/';";
-echo "let showIdModal = " .
-    ($_SESSION["show_id_modal"] ? "true" : "false") .
-    ";";
+$showIdModal = isset($_SESSION["show_id_modal"]) ? "true" : "false";
+echo "let showIdModal = {$showIdModal};";
 exit();
