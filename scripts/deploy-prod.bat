e8d2b10a5fa16acfea4d7f6d3be9b68289974b9b
marumaru-3 <yukinaka3009@gmail.com>
2025-04-18 08:31:40 +0900

    chore: 本番環境用のスクリプトを作成

---
+++ scripts/deploy-prod.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+chcp 65001
+
+echo === 本番環境用に差し替えます ===
+
+copy /Y ".htaccess.public" "public\.htaccess"
+
+git add public/.htaccess
+git commit -m "chore: 本番環境用の .htaccess に差し替え"
+git push heroku-prod main
+
+echo === 作業完了！お疲れさまでした ===
+pause
