272267299407260d0dacaab8525a9635f0cdacb4
marumaru-3 <yukinaka3009@gmail.com>
2025-06-15 01:05:03 +0900

    feat: 体重記録モーダルのUIを刷新して視認性を向上

--- php/views/modals/content/recordAdmin.php
+++ php/views/modals/content/recordAdmin.php
@@ -4,64 +4,67 @@ namespace view\modal\recordAdmin;
 
 function index()
 {
-    ?>
-<div class="modal-record-admin">
-  <div class="modal-record-admin__date">
-    <p class="date__contents"></p>
-  </div>
-  <form id="record-form"
-        class="record-form validate-form"
-        novalidate>
-    <div class="record-form__contents validate-form__input">
-      <div class="basic-info-form__box">
-        <label for="weight"
-               class="label-name">体重</label>
-        <div class="record-form__input">
-          <input type="text"
-                 id="weight"
-                 name="weight"
-                 placeholder="例： 68.4"
-                 step="0.1"
-                 inputmode="decimal"
-                 pattern="[0-9]+(\.[0-9]+)?"
-                 value=""
-                 data-float
-                 required> <span class="weight__unit">kg</span>
-        </div>
-        <div class="validate-text"></div>
-      </div>
+?>
+  <div class="modal-record-admin">
+    <div class="modal-record-admin__date">
+      <p class="date__contents"></p>
     </div>
-    <div class="record-form__contents">
-      <div class="basic-info-form__box">
-        <label for="memo"
-               class="label-name">一言メモ <span class="label-att">※30文字以内</span></label>
-        <div class="record-form__input">
-          <textarea id="memo"
-                    name="memo"
-                    rows="1"
-                    maxlength="30"
-                    placeholder="例: 今日はたくさん運動した。"></textarea>
+    <form id="record-form"
+      class="record-form validate-form"
+      novalidate>
+      <div class="record-form__contents validate-form__input">
+        <div class="basic-info-form__box">
+          <label for="weight">
+            <span class="label-name label-name__weight">体重</span>
+          </label>
+          <div class="record-form__input">
+            <input type="text"
+              id="weight"
+              name="weight"
+              placeholder="---.-"
+              maxlength="5"
+              step="0.1"
+              inputmode="decimal"
+              pattern="[0-9]+(\.[0-9]+)?"
+              value=""
+              data-float
+              required> <span class="weight__unit">kg</span>
+          </div>
+          <div class="validate-text"></div>
         </div>
       </div>
-    </div>
-    <input type="hidden"
-           id="recorded_at"
-           name="recorded_at"
-           value="">
+      <div class="record-form__contents">
+        <div class="basic-info-form__box">
+          <label for="memo">
+            <span class="label-name label-name__memo">一言メモ</span>
+            <span class="label-att">※30文字以内</span></label>
+          <div class="record-form__input">
+            <textarea id="memo"
+              name="memo"
+              rows="1"
+              maxlength="30"
+              placeholder="例: 今日はたくさん運動した。"></textarea>
+          </div>
+        </div>
+      </div>
+      <input type="hidden"
+        id="recorded_at"
+        name="recorded_at"
+        value="">
 
-    <div class="modal-buttons">
-      <button type="button"
-              id="delete-btn"
-              class="btn btn--att-color-01"><span class="btn__text">削除</span></button>
-      <div class="modal-buttons__group">
+      <div class="modal-buttons">
         <button type="button"
-                class="close-modal btn btn--cancel"><span class="btn__text">キャンセル</span></button>
-        <button type="submit"
-                class="btn btn--record submit-btn"><span class="btn__text">編集を保存</span></button>
+          id="delete-btn"
+          class="btn btn--att-color-01"><span class="btn__text">削除</span></button>
+        <div class="modal-buttons__group">
+          <button type="button"
+            class="close-modal btn btn--cancel"><span class="btn__text">キャンセル</span></button>
+          <button type="submit"
+            class="btn btn--record submit-btn"><span class="btn__text">編集を保存</span></button>
+        </div>
       </div>
-    </div>
-  </form>
-</div>
+    </form>
+  </div>
 <?php
 }
 ?>
