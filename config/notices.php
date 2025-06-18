d68cfebceee81eb975450da5c3ab8e6c8c26ea3c
marumaru-3 <yukinaka3009@gmail.com>
2025-06-18 23:33:03 +0900

    feat: 通知モーダルにモーダル高速化の改善内容を追記

--- config/notices.php
+++ config/notices.php
@@ -5,6 +5,19 @@ namespace config;
 function notices()
 {
   return [
+    "notice02" => [
+      'type' => 'info',
+      'title' => 'アップデートのお知らせ【6/19 09:00公開】',
+      'date' => '2025/06/19 09:00',
+      'body' =>
+      "<p>
+        6/19 09:00にアップデートを行いました。<br>
+        アップデート内容の詳細は下記のとおりです。<br>
+        <br>
+        <span class='indent'>・記録を追加モーダル及び記録を編集モーダルのUIを改善しました。</span>
+        <span class='indent'>・モーダル表示速度を改善しました。</span>
+      </p>"
+    ],
     "notice01" => [
       'type' => 'info',
       'title' => '「体重Web」本番リリースのお知らせ',
