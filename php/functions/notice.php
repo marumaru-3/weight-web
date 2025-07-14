e4f8ebe62d2b0aee91c01bf8cce58cc2a52d9cc0
marumaru-3 <yukinaka3009@gmail.com>
2025-07-14 20:15:16 +0900

    fix: notices が空配列のとき false を返すようガード追加

---
+++ php/functions/notice.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace lib;
+
+use DateTime;
+use db\NoticeReadQuery;
+use function config\notices;
+
+function hasUnreadNotice($user_id)
+{
+  $notices = notices();
+  if (empty($notices)) {
+    return false;
+  }
+  $latestNotice = new DateTime(max(array_column($notices, 'date')));
+  $noticeRead = NoticeReadQuery::fetchByUserId($user_id);
+  $lastRead = $noticeRead ? new DateTime($noticeRead->last_read_at) : null;
+
+  return is_null($lastRead) || $lastRead < $latestNotice;
+}
